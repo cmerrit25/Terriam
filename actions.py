d6e964c293303eaffc1c134ae2312aec2af225a1
Courtney Merritt <courtneymerrittdev@gmail.com>
2025-10-21 16:16:58 -0400

    basic training action logic. refactored classes for proper inheritance

--- actions.py
+++ actions.py
@@ -2,6 +2,7 @@ from utils import get_item, get_small_enemy, get_large_enemy, get_miniboss, get_
 import random
 from classes import GameState
 
+# defines possible findings on explore action
 def explore(gamestate: GameState):
 
     findings = {
@@ -18,6 +19,7 @@ def explore(gamestate: GameState):
     found = findings[find_options[explore]]
     plyr_choice(found, find_options[explore], gamestate)
 
+# adjust player object on item find
 def plyr_choice(item_found, item_type: str, gamestate: GameState):
     if item_type == "item":
         print(f"{item_found} was found!\n")
@@ -30,9 +32,14 @@ def plyr_choice(item_found, item_type: str, gamestate: GameState):
         else:
             print(f"{item_found} was not equipped...")
 
+# adjust player object on train action
+# rudimentary, need to insert minigame or something to reward on success with training xp
+def train(gamestate: GameState) -> None:
+    # always give a 4th of a level per training
+    train_xp = gamestate.player.xp / 4
+    gamestate.player.gain_xp(train_xp)
+    return
 
-def train(gamestate):
-    pass
-
-def fight(enemy, gamestate):
+# adjust player object on fight action
+def fight(enemy, gamestate: GameState):
     pass
--- classes.py
+++ classes.py
@@ -7,101 +7,6 @@ Need to make sure that balancing feels good as well.
 """
 
 import math
-
-class GameState:
-    def __init__(self, Player, Nemesis, boss_one=None, boss_two=None, mboss_one=None, mboss_two=None):
-        self.player = Player
-        self.nemesis = Nemesis
-        self.progress = 0
-        self.bosses_defeated = 0
-        if boss_one:
-            self.boss_one = boss_one
-        if boss_two:
-            self.boss_two = boss_two
-        if mboss_one:
-            self.mboss_one = mboss_one
-        if mboss_two:
-            self.mboss_two = mboss_two
-
-    def __str__(self):
-        player_stats = str(self.player)
-        nemesis_stats = str(self.nemesis)
-        plyr_boss_prog = f"The player has defeated {self.bosses_defeated} boss(es).\n"
-        if self.player > self.nemesis:
-            comp_strength = "The player is currently stronger than the nemesis.\n"
-        else:
-            comp_strength = "The player is currently weaker than the nemesis...\n"
-        return f"{player_stats}{nemesis_stats}{plyr_boss_prog}{comp_strength}"
-    
-    def equip_item(self, item):
-        player_stats = self.player.get_stats()
-        item_stats = item.get_stats()
-        for stat in item_stats.keys():
-            player_stats[stat] += stat
-
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.stats = {
-            "attack": 25,
-            "defense": 10,
-            "speed": 15,
-            "health": 75
-        }
-        
-        self.xp = 0
-        self.level = 1
-
-    # setter for player name
-    @name.setter
-    def name(self, value):
-        self._name = value
-
-    # getter for player name
-    @property
-    def name(self):
-        return self._name
-    
-    # getter for player stats
-    @property
-    def stats(self):
-        return self.stats
-
-    def __str__(self):
-        stats = f"{self.name}'s stats: Max HP - {self.health}, Attack - {self.attack}, Defense - {self.defense}, Speed - {self.speed}\n"
-        lvl_data = f"The player is level {self.level} and needs {self.calc_lvl_cost} to get to level {self.level + 1}!\n"
-        return f"{stats}{lvl_data}"
-    
-    def take_damage(self, damage):
-        player_death = False
-        self.health -= damage
-        if self.health <= 0:
-            player_death = True
-        return player_death
-
-    def gain_xp(self, xp_gained):
-        self.xp += xp_gained
-        self.level_up()
-
-    def calc_lvl_cost(self):
-        return 10 * math.pow(2, self.level - 1)
-    
-    def scale_stats(self):
-        for stat in self.stats.values():
-            stat *= 1.1
-
-    def level_up(self):
-        lvl_up_xp = self.calc_lvl_cost()
-        
-        while self.xp >= lvl_up_xp:
-            if self.xp == lvl_up_xp:
-                self.xp -= lvl_up_xp
-                self.level += 1
-                self.scale_stats()
-
-            lvl_up_xp = self.calc_lvl_cost()
-        
-        print(f"{self.name} has leveled up! Their new level is now {self.level}!")
     
 class Nemesis:
     def __init__(self, name):
@@ -183,3 +88,98 @@ class Reaver(Miniboss):
         self.armor  = int(self.armor * self.MULT["armor"])
         self.speed  = int(self.speed * self.MULT["speed"])
         self.attack = int(self.attack * self.MULT["attack"])
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.stats = {
+            "attack": 25,
+            "defense": 10,
+            "speed": 15,
+            "health": 75
+        }
+        
+        self.xp = 0
+        self.level = 1
+
+    # setter for player name
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    # getter for player name
+    @property
+    def name(self):
+        return self._name
+    
+    # getter for player stats
+    @property
+    def stats(self):
+        return self.stats
+
+    def __str__(self):
+        stats = f"{self.name}'s stats: Max HP - {self.health}, Attack - {self.attack}, Defense - {self.defense}, Speed - {self.speed}\n"
+        lvl_data = f"The player is level {self.level} and needs {self.calc_lvl_cost} to get to level {self.level + 1}!\n"
+        return f"{stats}{lvl_data}"
+    
+    def take_damage(self, damage):
+        player_death = False
+        self.health -= damage
+        if self.health <= 0:
+            player_death = True
+        return player_death
+
+    def gain_xp(self, xp_gained):
+        self.xp += xp_gained
+        self.level_up()
+
+    def calc_lvl_cost(self):
+        return 10 * math.pow(2, self.level - 1)
+    
+    def scale_stats(self):
+        for stat in self.stats.values():
+            stat *= 1.1
+
+    def level_up(self):
+        lvl_up_xp = self.calc_lvl_cost()
+        
+        while self.xp >= lvl_up_xp:
+            if self.xp == lvl_up_xp:
+                self.xp -= lvl_up_xp
+                self.level += 1
+                self.scale_stats()
+
+            lvl_up_xp = self.calc_lvl_cost()
+        
+        print(f"{self.name} has leveled up! Their new level is now {self.level}!")
+
+class GameState:
+    def __init__(self, player: Player, nemesis: Nemesis, boss_one=None, boss_two=None, mboss_one=None, mboss_two=None):
+        self.player = player
+        self.nemesis = nemesis
+        self.progress = 0
+        self.bosses_defeated = 0
+        if boss_one:
+            self.boss_one = boss_one
+        if boss_two:
+            self.boss_two = boss_two
+        if mboss_one:
+            self.mboss_one = mboss_one
+        if mboss_two:
+            self.mboss_two = mboss_two
+
+    def __str__(self):
+        player_stats = str(self.player)
+        nemesis_stats = str(self.nemesis)
+        plyr_boss_prog = f"The player has defeated {self.bosses_defeated} boss(es).\n"
+        if self.player > self.nemesis:
+            comp_strength = "The player is currently stronger than the nemesis.\n"
+        else:
+            comp_strength = "The player is currently weaker than the nemesis...\n"
+        return f"{player_stats}{nemesis_stats}{plyr_boss_prog}{comp_strength}"
+    
+    def equip_item(self, item):
+        player_stats = self.player.get_stats()
+        item_stats = item.get_stats()
+        for stat in item_stats.keys():
+            player_stats[stat] += stat
